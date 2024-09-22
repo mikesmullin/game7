4558e97e9d019421ab36959c575dd634b7669c26
Mike Smullin <mike@mikesmullin.com>
2024-09-21 23:32:38 -0600

    beginning of walls rendered in 2.5d way

--- src/lib/Bitmap3D.c
+++ src/lib/Bitmap3D.c
@@ -11,6 +11,111 @@ static u32 W;
 static u32 H;
 static u32 PS = 8;  // pixel super sample factor
 
+// Transforms 2D world coordinates into 3D camera space
+void transformToCameraSpace(f64 x, f64 y, f64* result) {
+  f64 xCamSpace = ((x - 0.5) - camX) * 2;
+  f64 yCamSpace = ((y - 0.5) - camY) * 2;
+
+  result[0] = xCamSpace * rCos - yCamSpace * rSin;
+  result[2] = yCamSpace * rCos + xCamSpace * rSin;
+}
+
+// Projects a 3D point onto the 2D screen
+void projectToScreen(f64 x, f64 z, f64* result) {
+  result[0] = (W / 2.0f) - (x / z * fov);
+}
+
+// Draws the wall between two projected points
+void drawWall(
+    f64* screenStart,
+    f64* screenEnd,
+    f64 zStart,
+    f64 zEnd,
+    f64 texCoordStart,
+    f64 texCoordEnd,
+    u32 tex,
+    u32 color,
+    Bitmap_t* bmp,
+    Bitmap_t* tbmp) {
+  u32 xStart = (u32)ceil(screenStart[0]);
+  u32 xEnd = (u32)ceil(screenEnd[0]);
+  if (xStart < 0) xStart = 0;
+  if (xEnd > W) xEnd = W;
+
+  f64 texScaleStart = texCoordStart * 16;
+  f64 texScaleEnd = texCoordEnd * 16;
+
+  f64 zInvStart = 1 / zStart;
+  f64 zInvEnd = 1 / zEnd;
+
+  f64 texInvStart = texScaleStart * zInvStart;
+  f64 texInvEnd = texScaleEnd * zInvEnd;
+
+  f64 xRangeInverse = 1 / (screenEnd[0] - screenStart[0]);
+
+  for (u32 x = xStart; x < xEnd; x++) {
+    f64 t = (x - screenStart[0]) * xRangeInverse;
+    f64 zInvCurrent = zInvStart + (zInvEnd - zInvStart) * t;
+
+    // if (zBufferWall[x] > zInvCurrent) continue;  // Skip if pixel is farther away
+    // zBufferWall[x] = zInvCurrent;
+
+    u32 textureX = (u32)((texInvStart + (texInvEnd - texInvStart) * t) / zInvCurrent);
+
+    // Calculate vertical bounds for the wall at this x-position
+    f64 yStart = ((-0.5) / zStart * fov + (H / 2.0f)) - 0.5;
+    f64 yEnd = ((+0.5) / zEnd * fov + (H / 2.0f));
+
+    u32 yPixelStart = (u32)ceil(yStart);
+    u32 yPixelEnd = (u32)ceil(yEnd);
+    if (yPixelStart < 0) yPixelStart = 0;
+    if (yPixelEnd > H) yPixelEnd = H;
+
+    f64 yRangeInverse = 1 / (yEnd - yStart);
+
+    for (u32 y = yPixelStart; y < yPixelEnd; y++) {
+      f64 yRatio = (y - yStart) * yRangeInverse;
+      u32 textureY = (u32)(16 * yRatio);
+      u32 texel = Bitmap__Get2DPixel(tbmp, textureX, textureY, 0xffff00ff);
+      Bitmap__Set2DPixel(bmp, x, y, texel * color);
+      // zBuffer[x + y * W] = 1 / zInvCurrent * 4;
+    }
+  }
+}
+
+void Bitmap3D__RenderWall(
+    Engine__State_t* game,
+    f64 x0,
+    f64 y0,
+    f64 x1,
+    f64 y1,
+    u32 tex,
+    u32 color,
+    f64 texCoordStart,
+    f64 texCoordEnd) {
+  f64 start[3], end[3];
+  transformToCameraSpace(x0, y0, start);
+  transformToCameraSpace(x1, y1, end);
+
+  f64 screenStart[2], screenEnd[2];
+  projectToScreen(start[0], start[2], screenStart);
+  projectToScreen(end[0], end[2], screenEnd);
+
+  if (screenStart[0] >= screenEnd[0]) return;
+
+  drawWall(
+      screenStart,
+      screenEnd,
+      start[2],
+      end[2],
+      texCoordStart,
+      texCoordEnd,
+      tex,
+      color,
+      &game->local->screen,
+      &game->local->atlas);
+}
+
 void Bitmap3D__RenderHorizon(Engine__State_t* game) {
   W = game->CANVAS_WIDTH;
   H = game->CANVAS_HEIGHT;
@@ -58,46 +163,8 @@ void Bitmap3D__RenderHorizon(Engine__State_t* game) {
     }
   }
 
-  Bitmap3D__RenderWall(game, 1, 1, 2, 2);
+  Bitmap3D__RenderWall(game, 1, 0, 1, 1, 0, 0xffff00ff, 0, 0);
   Bitmap3D__RenderFloor(game);
-}
-
-void Bitmap3D__RenderWall(Engine__State_t* game, s32 x0, s32 y0, s32 x1, s32 y1) {
-  s32 x, y;
-  u32 color = 0xffff00ff;
-  u32 ts = 8;    // tile size
-  f32 d = 4.0f;  // tile size
-  u32* buf = (u32*)game->local->screen.buf;
-  u32 len = game->local->screen.len;
-  f32 yd = 0, zd = 0, xd = 0;
-
-  for (y = 0; y < H; y++) {
-    yd = ((y / (f32)H) * 2) - 1;
-    f32 ydd = yd;
-    if (ydd < 0) {  // ceiling is mirrored, not flipped
-      ydd = -ydd;
-    }
-    yd *= PS;
-
-    zd = (camZ * PS) / yd;
-    if (yd < 0) {  // ceiling is mirrored, not flipped
-      zd = -zd;
-    }
-
-    for (x = 0; x < W; x++) {
-      xd = ((x / (f32)W) * 2) - 1;
-      xd *= zd;
-
-      u32 xx = (u32)(((xd * rSin) + (zd * rCos)) + camX) & atlas_tile_size;
-      u32 yy = (u32)(((xd * rCos) - (zd * rSin)) + camY) & atlas_tile_size;
-      if (xx >= x0 && xx <= x1 && yy >= y0 && yy <= y1) {
-        // color = ((u32*)game->local->atlas.buf)[(xx + yy * atlas_dim) % game->local->atlas.len];
-        // but i instead want to write to a projected coordinate
-        color = 0xffff00ff;  // pink
-        buf[(x + y * W) % len] = color;
-      }
-    }
-  }
 }
 
 void Bitmap3D__RenderFloor(Engine__State_t* game) {
@@ -133,14 +200,13 @@ f64 easeMike(f32 t, f32 s) {
 }
 
 void Bitmap3D__PostProcessing(Engine__State_t* game) {
-  return;
   u32* buf = (u32*)game->local->screen.buf;
   f32* zbuf = game->local->zbuf;
   for (u32 i = 0; i < W * H; i++) {
     f32 brightness = zbuf[i];
-    f32 s = Math__map(Math__sin((game->local->currentTime / 1000)), -1, 1, 0, 1);
+    // f32 s = Math__map(Math__sin((game->local->currentTime / 1000)), -1, 1, 0, 1);
     // brightness *= easeInQuart(s);
-    brightness = easeMike(brightness, 10 * easeInQuart(s));
+    brightness = easeMike(brightness, 10 * easeInQuart(0.5f));
     buf[i] = ((u32)(buf[i] * brightness) & 0xff000000) | (buf[i] & 0x00ffffff);
   }
 }
--- src/lib/Bitmap3D.h
+++ src/lib/Bitmap3D.h
@@ -6,7 +6,6 @@
 void Bitmap3D__Perspective(f32 fov, f32 aspect, f32 nearZ, f32 farZ, mat4 result);
 void Bitmap3D__RenderHorizon(Engine__State_t* game);
 void Bitmap3D__RenderFloor(Engine__State_t* game);
-void Bitmap3D__RenderWall(Engine__State_t* game, s32 x0, s32 y0, s32 x1, s32 y1);
 void Bitmap3D__PostProcessing(Engine__State_t* game);
 
 #endif  // BITMAP3D_H
