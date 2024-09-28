9d873c2a18e09f08df3bf64eb15fd487ac5406aa
Mike Smullin <mike@mikesmullin.com>
2024-09-28 11:18:39 -0600

    initial List impl

---
+++ src/lib/List.c
@@ -0,0 +1,49 @@
+#include "List.h"
+
+#include "Arena.h"
+
+List_t* List__alloc(Arena_t* arena) {
+  return Arena__Push(arena, sizeof(List_t));
+}
+
+List__Node_t* List__Node__alloc(Arena_t* arena) {
+  return Arena__Push(arena, sizeof(List__Node_t));
+}
+
+void List__init(List_t* list) {
+  list->len = 0;
+  list->head = NULL;
+  list->tail = NULL;
+}
+
+void List__Node__init(List__Node_t* node, void* data) {
+  node->data = data;
+  node->next = NULL;
+}
+
+void List__append(Arena_t* arena, List_t* list, void* data) {
+  List__Node_t* node = List__Node__alloc(arena);
+  List__Node__init(node, data);
+
+  if (NULL == list->tail) {
+    list->head = node;
+    list->tail = node;
+  } else {
+    list->tail->next = node;
+    list->tail = node;
+  }
+  list->len++;
+}
+
+void* List__get(List_t* list, u32 index) {
+  List__Node_t* c = list->head;
+  u32 i = 0;
+  while (NULL != c) {
+    if (i == index) {
+      return c->data;
+    }
+    i++;
+    c = c->next;
+  }
+  return NULL;
+}
---
+++ src/lib/List.h
@@ -0,0 +1,27 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <stdint.h>
+typedef uint32_t u32;
+
+typedef struct Arena_t Arena_t;
+
+typedef struct List__Node_t {
+  struct List__Node_t* next;
+  void* data;
+} List__Node_t;
+
+typedef struct List_t {
+  u32 len;
+  List__Node_t* head;
+  List__Node_t* tail;
+} List_t;
+
+List_t* List__alloc(Arena_t* arena);
+List__Node_t* List__Node__alloc(Arena_t* arena);
+void List__init(List_t* list);
+void List__Node__init(List__Node_t* node, void* data);
+void List__append(Arena_t* arena, List_t* list, void* data);
+void* List__get(List_t* list, u32 index);
+
+#endif  // LIST_H
