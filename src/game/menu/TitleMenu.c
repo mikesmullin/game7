088fcd16f24f4e685a604294bc0260f445d61006
Mike Smullin <mike@mikesmullin.com>
2024-09-27 18:03:29 -0600

    forgot header counts as forward declaration so I can reorder these

--- src/game/menu/TitleMenu.c
+++ src/game/menu/TitleMenu.c
@@ -8,6 +8,12 @@ Menu_t* TitleMenu__alloc(Engine__State_t* state) {
   return Arena__Push(state->arena, sizeof(TitleMenu_t));
 }
 
+void TitleMenu__init(Menu_t* menu, Engine__State_t* state) {
+  menu->tick = &TitleMenu__tick;
+  menu->render = &TitleMenu__render;
+  state->Vulkan__FReadImage(&((TitleMenu_t*)menu)->bmp, "../assets/textures/title.png");
+}
+
 void TitleMenu__tick(struct Menu_t* menu, void* _state) {
   Engine__State_t* state = _state;
 
@@ -19,10 +25,4 @@ void TitleMenu__tick(struct Menu_t* menu, void* _state) {
 
 void TitleMenu__render(struct Menu_t* menu, Bitmap_t* target) {
   Bitmap__Draw(&((TitleMenu_t*)menu)->bmp, target, 0, 0);
-}
-
-void TitleMenu__init(Menu_t* menu, Engine__State_t* state) {
-  menu->tick = &TitleMenu__tick;
-  menu->render = &TitleMenu__render;
-  state->Vulkan__FReadImage(&((TitleMenu_t*)menu)->bmp, "../assets/textures/title.png");
 }
